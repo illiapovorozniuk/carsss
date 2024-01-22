3d16e0de7313a7e734cf1ea20af0d55e25f4a540
Illia <ipz211_piv@student.ztu.edu.ua>
2024-01-22 20:54:05 +0200

    Add login to api and to vue

--- back/app/Http/Controllers/AuthController.php
+++ back/app/Http/Controllers/AuthController.php
@@ -4,15 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\Rules\Password;
 
 class AuthController extends Controller
 {
-    public  function register(Request $request){
+    public function register(Request $request)
+    {
         $data = $request->validate([
-            'name'=>'required|string',
-            'email'=>'required|email|string|unique:users,email',
-            'password'=>[
+            'name' => 'required|string',
+            'email' => 'required|email|string|unique:users,email',
+            'password' => [
                 'required',
                 'confirmed',
                 Password::min(8)->mixedCase()->numbers()->symbols()
@@ -20,11 +22,35 @@ class AuthController extends Controller
         ]);
         /** @var User $user */
         $user = User::create([
-            'name'=>$data['name'],
-            'email'=>$data['email'],
-            'password'=>$data['password'],
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => $data['password'],
         ]);
 
+        $token = $user->createToken('main')->plainTextToken;
+
+        return response([
+            'user' => $user,
+            'token' => $token
+        ]);
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email'=> 'required|email|string|exists:users,email',
+            'password'=>[
+                'required',
+            ],
+        ]);
+        if(!Auth::attempt($credentials,true)){
+            return response([
+                'error'=> 'The Provided credentials are not correct'
+            ],422);
+        }
+
+        /** @var User $user */
+        $user = Auth::user();
         $token = $user->createToken('main')->plainTextToken;
 
         return response([
@@ -32,7 +58,4 @@ class AuthController extends Controller
             'token'=>$token
         ]);
     }
-
-//    public function login(Request $request){
-//    }
 }
--- front/src/views/Login.vue
+++ front/src/views/Login.vue
@@ -6,11 +6,20 @@
   </div>
 
   <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-    <form class="space-y-6" action="#" method="POST">
+    <form class="space-y-6" @submit="login" method="POST">
+      <div v-if="errorMsg" class="flex items-center justify-between py-3  px-5 bg-red-500 text-white rounded">
+        {{ errorMsg }}
+        <span @click="errorMsg=''" class = "w-8 h-8 flex items-center justify-center rounded-full transition-colors hover:bg-[rgba(0,0,0,0.2)]">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+               stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12"/>
+</svg>
+</span>
+      </div>
       <div>
         <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
         <div class="mt-2">
-          <input id="email" name="email" type="email" autocomplete="email" required=""
+          <input id="email" name="email" type="email" autocomplete="email" required="" v-model="user.email"
                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
         </div>
       </div>
@@ -21,6 +30,7 @@
         </div>
         <div class="mt-2">
           <input id="password" name="password" type="password" autocomplete="current-password" required=""
+                 v-model="user.password"
                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
         </div>
       </div>
@@ -43,10 +53,34 @@
   </div>
 </template>
 
-<script>
-export default {
-  name: "Login"
+<script setup>
+import store from "../store/index.js";
+import {useRouter} from "vue-router";
+import {ref} from "vue";
+
+const router = useRouter();
+
+const user = {
+  email: '',
+  password: ''
 }
+
+let errorMsg = ref('');
+
+function login(ev) {
+  ev.preventDefault();
+
+  store.dispatch('login', user).then(() => {
+    router.push({
+      name: "Dashboard"
+    })
+  }).catch(err => {
+        errorMsg.value = err.response.data.error
+      }
+  )
+}
+
+
 </script>
 
 <style lang="scss" scoped>
