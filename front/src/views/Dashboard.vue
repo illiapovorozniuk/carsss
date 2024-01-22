cb92c35ae22081e9b21a877e0ff8675759b328f4
Illia <ipz211_piv@student.ztu.edu.ua>
2024-01-22 13:14:11 +0200

    Add page component

--- front/src/views/Dashboard.vue
+++ front/src/views/Dashboard.vue
@@ -1,18 +1,10 @@
 <template>
-  <header class="bg-white shadow">
-    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-      <h1 class="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
-    </div>
-  </header>
-  <main>
-    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-      Content
-    </div>
-  </main>
+  <PageComponent title = "Dashboard">Content</PageComponent>
 </template>
 
-<script>
-export default {}
+<script setup>
+import PageComponent from "../components/PageComponent.vue";
+
 </script>
 
 <style lang="scss" scoped>
--- front/src/views/Surveys.vue
+++ front/src/views/Surveys.vue
@@ -1,38 +1,11 @@
 <template>
-  <header class="bg-white shadow">
-    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-      <h1 class="text-3xl font-bold tracking-tight text-gray-900">All surveys</h1>
-    </div>
-  </header>
-  <main>
-    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-      Content goes here
-    </div>
-  </main>
+<PageComponent title="All surveys">Content</PageComponent>
  </template>
  
  <script setup>
- import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
- import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/vue/24/outline'
- 
- const user = {
-   name: 'Tom Cook',
-   email: 'tom@example.com',
-   imageUrl:
-     'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
- }
- const navigation = [
-   { name: 'Dashboard', href: '#', current: true },
-   { name: 'Team', href: '#', current: false },
-   { name: 'Projects', href: '#', current: false },
-   { name: 'Calendar', href: '#', current: false },
-   { name: 'Reports', href: '#', current: false },
- ]
- const userNavigation = [
-   { name: 'Your Profile', href: '#' },
-   { name: 'Settings', href: '#' },
-   { name: 'Sign out', href: '#' },
- ]
+
+
+ import PageComponent from "../components/PageComponent.vue";
  </script>
 
 <script>
