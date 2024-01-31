dcf65c117356ec48135c3244814d4f206b49944e
Illia <ipz211_piv@student.ztu.edu.ua>
2024-01-31 10:58:29 +0200

    implement pagination

---
+++ front/src/components/Pagination.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { FwbPagination } from 'flowbite-vue'
+import { ref } from 'vue'
+
+const props = defineProps({
+  total_pages: Number,
+  slice_length: Number
+
+});
+const currentPage = ref(1)
+</script>
+<template>
+  <fwb-pagination class="bg-gray-100 max-w-50" v-model="currentPage" :total-pages=3 :slice-length="4"></fwb-pagination>
+</template>
--- front/src/components/StatisticsTable.vue
+++ front/src/components/StatisticsTable.vue
@@ -18,14 +18,20 @@
             </tr>
             </thead>
             <tbody>
-            <tr :class="[index%2===0?'bg-gray-100 border-b':'']" v-for="(car_row,index) in tableData">
+            <tr :class="[index%2===0?'bg-gray-100 border-b':'']" v-for="(car_row,index) in paginatedTableData">
               <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{{ car_row.car_id }}</td>
-              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">{{ car_row.registration_number }}</td>
+              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">{{
+                  car_row.registration_number
+                }}
+              </td>
               <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">{{ car_row.brand_slug }}</td>
               <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">{{ car_row.car_slug }}</td>
               <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">{{ car_row.color }}</td>
               <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">{{ car_row.schedule.free }}</td>
-              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">{{ car_row.schedule.service }}</td>
+              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">{{
+                  car_row.schedule.service
+                }}
+              </td>
               <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">{{ car_row.schedule.busy }}</td>
               <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">{{ car_row.schedule.days }}</td>
 
@@ -35,15 +41,42 @@
           </table>
         </div>
       </div>
+      <div class="flex items-center justify-center mt-4">
+        <div class="inline-block"> <Pagination :totalPages="totalPages"
+                                               :currentPage="currentPage"
+                                               @pageChanged="changePage"></Pagination></div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import Pagination from "./Pagination.vue";
 export default {
+  components: { Pagination },
   props: ["tableData"],
-}
-// console.log(this.tableData);
+  data() {
+    return {
+      currentPage: 1,
+      itemsPerPage: 12, // COUNT OF ROWS PER PAGE
+    };
+  },
+  computed: {
+    paginatedTableData() {
+      const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+      const endIndex = startIndex + this.itemsPerPage;
+      return this.tableData.slice(startIndex, endIndex);
+    },
+    totalPages() {
+      return Math.ceil(this.tableData.length / this.itemsPerPage);
+    },
+  },
+  methods: {
+    changePage(newPage) {
+      this.currentPage = newPage;
+    },
+  },
+};
 </script>
 
 <style scoped lang="scss">
